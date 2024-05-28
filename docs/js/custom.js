94bc8cc1979a2a4604d2ad255a6061ea0df52dea
Dražen L <drazen@altinet.hr>
2024-05-28 10:34:38 +0200

    commit

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -1,16 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var downloadButton = document.createElement('div');
-    downloadButton.id = 'downloadPDF';
-    downloadButton.innerHTML = '<img src="../images/pdf.png" style="width:50px;height:50px;vertical-align:middle"/> Preuzmi kao PDF';
-    downloadButton.style = 'font-size:16px; padding:10px 20px; cursor:pointer; text-align:center;';
-    downloadButton.setAttribute('data-filename', document.title + '.pdf');
-    downloadButton.classList.add('no-print');
-    
-    document.body.appendChild(downloadButton);
-    
-    downloadButton.addEventListener('click', function() {
-        var element = document.querySelector('.md-content'); 
-        var filename = this.getAttribute('data-filename');
-        html2pdf().from(element).save(filename);
-    });
+    var mdContent = document.querySelector('.md-content');
+    if (mdContent) {
+        var downloadButton = document.createElement('div');
+        downloadButton.id = 'downloadPDF';
+        downloadButton.innerHTML = '<img src="../images/pdf.png" style="width:50px;height:50px;vertical-align:middle"/> Preuzmi kao PDF';
+        downloadButton.style = 'font-size:16px; padding:10px 20px; cursor:pointer; text-align:center;';
+        downloadButton.setAttribute('data-filename', document.title + '.pdf');
+        downloadButton.classList.add('no-print');
+        
+        mdContent.appendChild(downloadButton);
+        
+        downloadButton.addEventListener('click', function() {
+            var filename = this.getAttribute('data-filename');
+            html2pdf().from(mdContent).save(filename);
+        });
+    }
 });
